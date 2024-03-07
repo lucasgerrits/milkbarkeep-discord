9b0b29dab458c03dfdb94835fe5b0f44b23bdaf1
lucasgerrits <lucasjgerrits@gmail.com>
2024-03-07 09:28:35 -0600

    Add cows-leaderboard and cows-stats slash commands

---
+++ src/commands/stream/cowsLeaderboard.ts
@@ -0,0 +1,47 @@
+import { EmbedBuilder } from "discord.js";
+import { Command } from "../../classes/Command";
+import { Timestamps } from "../../classes/Timestamps";
+import urls from "../../../data/urls.json";
+
+export default new Command({
+    name: "cows-leaderboard",
+    description: "Displays data about the cow launches on CFB's stream.",
+    run: async (args): Promise<void> => {
+        await args.interaction.deferReply();
+
+        await fetch(urls.cowLeaderboard)
+            .then(async (response) => {
+                const text: string = await response.text();
+
+                const data: Array<{
+                    rank: string,
+                    user_name: string,
+                    launches: string,
+                    most_recent: string,
+                }> = JSON.parse(text);
+
+                let formattedText: string = "";
+                data.forEach((row) => {
+                    const timestamp: string = Timestamps.relative(new Date(row.most_recent));
+                    formattedText += `${row.rank}. [${row.user_name}](https://www.twitch.tv/${row.user_name})` +
+                        `: ${row.launches}\t(${timestamp})\n`;
+                });
+                
+                const embed: EmbedBuilder = new EmbedBuilder()
+                    .setColor("#000000")
+                    .setTitle("Cow Launch Top 10:")
+                    .setDescription(formattedText)
+                    .setThumbnail(urls.cowImg);
+
+                await args.interaction.editReply({
+                    embeds: [ embed ],
+                });
+            })
+            .catch(async (err) => {
+                console.log(err);
+                await args.interaction.editReply({
+                    content: "Something went wrong with your request."
+                });
+            });
+    }
+});
---
+++ src/commands/stream/cowsStats.ts
@@ -0,0 +1,72 @@
+import { ApplicationCommandOptionType, EmbedBuilder } from "discord.js";
+import { Command } from "../../classes/Command";
+import { Timestamps } from "../../classes/Timestamps";
+import urls from "../../../data/urls.json";
+
+export default new Command({
+    name: "cows-stats",
+    description: "Displays data about a user's cow launches on CFB's stream.",
+    options: [
+        {
+            name: "user",
+            description: "The twitch user to lookup.",
+            type: ApplicationCommandOptionType.String,
+            required: true
+        }
+    ],
+    run: async (args): Promise<void> => {
+        await args.interaction.deferReply();
+
+        const userArg: string = args.options.getString("user") ?? "";
+        const url: string = urls.cowUserStats + userArg;
+
+        await fetch(url)
+            .then(async (response) => {
+                const text: string = await response.text();
+
+                let formattedText: string = "";
+
+                if (text.substring(0, 12) === "User has not") {
+                    formattedText = `
+                        **User:** [\`${userArg}\`](https://www.twitch.tv/${userArg})
+                        ${text}
+                    `;
+                } else {
+                    // These shouldn't all be strings, it's an issue on the php end
+                    const data: {
+                        rank: string,
+                        launches: string,
+                        most_recent: string,
+                        total_users: string,
+                        total_launches: string,
+                    } = JSON.parse(text);
+                    const percentage: string = ((parseInt(data.launches) / parseInt(data.total_launches)) * 100).toFixed(2);
+                    const timestamp: string = Timestamps.default(new Date(data.most_recent));
+
+                    formattedText = `
+                        **User:** [\`${userArg}\`](https://www.twitch.tv/${userArg})
+                        **Rank:** ${data.rank} (of ${data.total_users})
+                        **Launches:** ${data.launches}
+                        **Contribution:** ${percentage}% (of ${data.total_launches})
+                        **Most Recent:** ${timestamp}
+                    `;
+                }
+
+                const embed: EmbedBuilder = new EmbedBuilder()
+                    .setColor("#000000")
+                    .setTitle(`Cow Launch User Stats`)
+                    .setDescription(formattedText)
+                    .setThumbnail(urls.cowImg);
+
+                await args.interaction.editReply({
+                    embeds: [ embed ],
+                });
+            })
+            .catch(async (err) => {
+                console.log(err);
+                await args.interaction.editReply({
+                    content: "Something went wrong with your request."
+                });
+            });
+    }
+});
