9abdc60acc8e3a45302ad527b2c1e307c56f11e9
lucasgerrits <lucasjgerrits@gmail.com>
2024-03-25 16:42:30 -0500

    Add new Convert methods

--- src/util/Convert.ts
+++ src/util/Convert.ts
@@ -1,9 +1,26 @@
 export class Convert {
-    public static cToF(c: number): number {
-        return ((9 / 5) * c) + 32;
+    // TEMPERATURE
+    public static cToF(celsius: number): number {
+        return ((9 / 5) * celsius) + 32;
     }
 
-    public static fToC(f: number): number {
-        return (f - 32) * (5 / 9);
+    public static fToC(fahrenheit: number): number {
+        return (fahrenheit - 32) * (5 / 9);
+    }
+
+    // ACCELERATION
+    public static mpsToMph(metersPerSecond: number): number {
+        return metersPerSecond * 2.23694;
+    }
+
+    public static mphToMps(milesPerHour: number): number {
+        return milesPerHour * 0.44704;
+    }
+
+    // DIRECTION
+    public static degreesToCompass(degrees: number): string {
+        const directions: Array<string> = ["N", "NE", "E", "SE", "S", "SW", "W", "NW", "N"];
+        const index: number = Math.round(degrees / 45) % 8;
+        return directions[index];
     }
 }
