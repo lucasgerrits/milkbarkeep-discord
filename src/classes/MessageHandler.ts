3f9a7da9d0ab7938d2ff9d12026735f5e55da468
lucasgerrits <lucasjgerrits@gmail.com>
2024-08-15 05:56:13 -0500

    Move message checking logic from messageCreate event to MessageHandler class

--- src/classes/MessageHandler.ts
+++ src/classes/MessageHandler.ts
@@ -1,9 +1,31 @@
-import { Message } from "discord.js";
+import { Message, User } from "discord.js";
+import { client } from "..";
+import channelIDs from "../../data/channelIDs.json";
+
 
 export class MessageHandler{
     constructor() {}
 
-    public async chiliDogCheck(message: Message): Promise<void> {
+    public async checkMessage(message: Message): Promise<void> {
+        if (message.channel.id === channelIDs.anonymous) return;
+        if (message.channel.id === channelIDs.chiliDog) {
+            // this.messageHandler.chiliDogCheck(message);
+        }
+        this.milkCheck(message);
+        this.tipCheck(message);
+
+        if (message.mentions.has(client.user as User)) {
+            /** OpenAI API stopped giving free trials, disabling until a better solution is researched
+            // API call
+            const openAI = new OpenAIApi();
+            const response = await openAI.chat(message);
+            // Reply
+            await message.reply(response);
+            */
+        }
+    }
+
+    private async chiliDogCheck(message: Message): Promise<void> {
         // const regex = new RegExp("^(<:cfbOrb:950146443170164767>){1}$");
         const regex: RegExp = new RegExp("^(suckin on a chili dog){1}$");
         if (regex.test(message.content) === false) {
@@ -11,7 +33,7 @@ export class MessageHandler{
         }
     }
 
-    public async milkCheck(message: Message): Promise<void> {
+    private async milkCheck(message: Message): Promise<void> {
         const melkEmote: string = "melk:616025879830855681";
         const milkStrings: string[] = ["milk", "melk", "malk", "mork"];
         const milkRegex: RegExp = new RegExp(milkStrings.join("|"), "i");
@@ -21,7 +43,8 @@ export class MessageHandler{
         }
     }
 
-    public async tipCheck(message: Message): Promise<void> {
+    /* Convert this to some kind of chat command or macro manager */
+    private async tipCheck(message: Message): Promise<void> {
         const regex: RegExp = /!tip/;
         const response: string = "<:cfbANY:1201985868319948831> ???\nhttps://streamelements.com/carefreebomb/tip";
 
--- src/events/messageCreate.ts
+++ src/events/messageCreate.ts
@@ -1,29 +1,11 @@
-import { Events, Message, User } from "discord.js";
+import { Events, Message } from "discord.js";
 import { Event } from "../classes/Event";
 import { client } from "..";
-import channelIDs from "../../data/channelIDs.json";
-import { OpenAIApi } from "../integrations/OpenAI-API";
+
 
 export default new Event(
     Events.MessageCreate,
     async (message: Message) => {
-        if (message.channel.id === channelIDs.anonymous) return;
-        if (message.channel.id === channelIDs.chiliDog) {
-            // this.messageHandler.chiliDogCheck(message);
-        }
-        client.messageHandler.milkCheck(message);
-        client.messageHandler.tipCheck(message);
-
-        if (message.mentions.has(client.user as User)) {
-            /** OpenAI API stopped giving free trials, disabling until a better solution is researched
-            // API call
-            const openAI = new OpenAIApi();
-            const response = await openAI.chat(message);
-            // Reply
-            await message.reply(response);
-            */
-        }
-
-
+        client.messageHandler.checkMessage(message);
     }
 );
