e83d74b91a1562e53ef982e950392bf874ed4287
lucasgerrits <lucasjgerrits@gmail.com>
2025-03-01 11:03:48 -0600

    Modify RA ranking slash command to use new embed class

--- src/commands/ra/ranking.ts
+++ src/commands/ra/ranking.ts
@@ -1,6 +1,7 @@
 import { ApplicationCommandOptionType, CommandInteraction, EmbedBuilder } from "discord.js";
 import { Command } from "../../core/Command";
-import type { userPoints } from "../../types/RATypes";
+import { RADiscordEmbeds } from "../../integrations/RADiscordEmbeds";
+import type { RARankingType } from "../../types/RATypes";
 
 export default new Command({
     name: "ra-ranking",
@@ -22,44 +23,21 @@ export default new Command({
     ],
     run: async (args): Promise<void> => {
         await args.interaction.deferReply();
-
-        // FUNCTIONS
-
-        const createString = async (list: userPoints[]) => {
-            const profileURL = "https://www.retroachievements.org/user/";
-            let output = "";
-            for (let i = 0; i < list.length; i++) {
-                output += `${i + 1}. [${list[i].username}]` +
-                    `(${profileURL}${list[i].username}): ` +
-                    `${list[i].points}\n`;
-            }
-            return output;
-        };
-
-        const createEmbed = async (label: string, descriptStr: string) => {
-            const thumbnail: string = "https://static.retroachievements.org/assets/images/ra-icon.webp";
-            const embed: EmbedBuilder = new EmbedBuilder()
-                .setColor("#cc9900")
-                .setThumbnail(thumbnail)
-                .setTitle(label)
-                .setDescription(descriptStr);
-            return embed;
-        };
-
-        // GET DATA
-
-        let label: string = "";
-        let embedString: string = "";
+        
         try {
-            if (args.options.getSubcommand() === "all-time") {
-                label = "All-Time";
-                embedString = await createString(await args.client.ra.getAllTimeList());
-            } else if (args.options.getSubcommand() === "daily") {
-                label = "Daily";
-                embedString = await createString(await args.client.ra.getDailyList(true));
-            } else if (args.options.getSubcommand() === "weekly") {
-                label = "Weekly";
-                embedString = await createString(await args.client.ra.getWeeklyList(true));
+            const listType: RARankingType = args.options.getSubcommand() as RARankingType;
+            const embed: EmbedBuilder = await RADiscordEmbeds.createRankingEmbed(args.client, listType);
+
+            try {
+                await args.interaction.editReply({
+                    content: "Bombsquad RA Users Ranking",
+                    embeds: [ embed ],
+                });
+            } catch (error) {
+                console.log(error);
+                await (args.interaction as CommandInteraction).editReply({
+                    content: "Something went wrong with your request.",
+                });
             }
         } catch (error) {
             const errorStr: string = "My apologies, but there may currently be an issue with the RA services. " +
@@ -67,23 +45,6 @@ export default new Command({
                 "https://discord.com/channels/310192285306454017/357633571307126784";
             await args.interaction.editReply({
                 content: errorStr,
-            });
-        }
-
-        // SEND
-
-        try {
-            await args.interaction.editReply({
-                content: "Bombsquad RA Users Ranking",
-                embeds: [ await createEmbed(
-                    label,
-                    embedString,
-                )],
-            });
-        } catch (error) {
-            console.log(error);
-            await (args.interaction as CommandInteraction).editReply({
-                content: "Something went wrong with your request.",
             });
         }
     }
