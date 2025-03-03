80b212f92602ba93af7c6fc317f7e4634a76dfa1
lucasgerrits <lucasjgerrits@gmail.com>
2025-03-03 16:43:48 -0600

    Modify setAvatar slash command to require attachment

--- src/commands/bot/setAvatar.ts
+++ src/commands/bot/setAvatar.ts
@@ -1,24 +1,35 @@
-import { ClientUser, PermissionFlagsBits } from "discord.js";
+import { ApplicationCommandOptionType, Attachment, ClientUser, MessageFlags, PermissionFlagsBits } from "discord.js";
 import { Command } from "../../core/Command";
+import { Logger } from "../../util/Logger";
 
 export default new Command({
     name: "set-avatar",
     description: "Sets the avatar of the bot.",
     defaultMemberPermissions: PermissionFlagsBits.Administrator,
+    options: [
+        {
+            name: "file",
+            description: "The image to be uploaded.",
+            type: ApplicationCommandOptionType.Attachment,
+            required: true
+        }
+    ],
     run: async (args): Promise<void> => {
-        await args.interaction.deferReply();
+        await args.interaction.deferReply({ flags: MessageFlags.Ephemeral });
 
-        const file: string = "pfpAnimated.gif";
-        const dir: string = `${__dirname}/../../../assets/`;
-        const fileName: string = dir + file;
+        //const defaultFile: string = "pfpAnimated.gif";
+        //const defaultDir: string = `${__dirname}/../../../assets/`;
+        //const defaultFileName: string = defaultDir + defaultFile;
+
+        const attachment = args.interaction.options.get("file", true)?.attachment as Attachment;
         
         const clientUser: ClientUser = args.client.user as ClientUser;
 
         try {
-            await clientUser.setAvatar(fileName);
+            await clientUser.setAvatar(attachment.url);
             await args.interaction.editReply({ content: "Client avatar set successfully." });
-        } catch (error) {
-            console.log(error);
+        } catch (error: any) {
+            Logger.log(error as string);
             await args.interaction.editReply({ content: "There was a problem with setting the client avatar." });
         }
     }
