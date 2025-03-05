18dd2f50cf1f6bc9667b3b7331f2a80a33c9b077
lucasgerrits <lucasjgerrits@gmail.com>
2025-03-05 09:51:37 -0600

    Add DiscordAPIError cases to emoteCopy

--- src/commands/emote/emoteCopy.ts
+++ src/commands/emote/emoteCopy.ts
@@ -1,4 +1,4 @@
-import { ApplicationCommandOptionType, Collection, EmbedBuilder, Guild, GuildEmoji, Message, MessageFlags, PermissionFlagsBits, TextChannel } from "discord.js";
+import { ApplicationCommandOptionType, Collection, DiscordAPIError, EmbedBuilder, Guild, GuildEmoji, Message, PermissionFlagsBits, TextChannel } from "discord.js";
 import { Command } from "../../core/Command";
 import { Logger } from "../../util/Logger";
 
@@ -78,36 +78,49 @@ export default new Command({
         const extension: string = isAnimated ? ".gif" : ".png";
         const fullUrl: string = `${cdnUrl}${emoteId}${extension}`;
 
+        // Get the image file as a buffer
+        let buffer: Buffer;
         try {
-            // Get the image file as a buffer
             const response: Response = await fetch(fullUrl);
-            if (!response.ok) {
-                throw new Error(`Failed to fetch emoji: ${response.statusText}`);
-            }
-            const buffer: Buffer = Buffer.from(await response.arrayBuffer());
-
-            // Upload to the guild
-            try {
-                const newEmote: GuildEmoji = await guild.emojis.create({
-                    attachment: buffer,
-                    name: emoteName
-                });
-                const newEmoteString: string = `<${(isAnimated ? "a" : "")}:${emoteName}:${newEmote.id}>`;
-
-                const embed: EmbedBuilder = new EmbedBuilder()
-                    .setColor("#000000")
-                    .setTitle(`:${emoteName}:`)
-                    .setThumbnail(fullUrl)
-                    .setDescription("Emote successfully copied to server.");
-                
-                Logger.log(`Successfully uploaded emote :${emoteName}: (${emoteId}) to guild ${guild.name} (${guildId})`);
-                await args.interaction.editReply({ embeds: [ embed ] });
-            } catch (error: any) {
-                throw new Error(`Failed to upload emote :${emoteName}: (${emoteId}) to guild ${guild.name} (${guildId})`);
-            }
-        } catch (error: any) {
+            if (!response.ok) { throw new Error(`Failed to fetch emoji: ${response.statusText}`); }
+            buffer = Buffer.from(await response.arrayBuffer());
+        } catch(error: any) {
             Logger.log(error as string);
-            await args.interaction.editReply({ content: "Something went wrong with copying the emote." });
+            await args.interaction.editReply({ content: "There was an issue with copying the emote image." });
+            return;
+        }
+
+        // Upload image to the guild
+        try {
+            const newEmote: GuildEmoji = await guild.emojis.create({
+                attachment: buffer,
+                name: emoteName
+            });
+            //const newEmoteString: string = `<${(isAnimated ? "a" : "")}:${emoteName}:${newEmote.id}>`;
+
+            const embed: EmbedBuilder = new EmbedBuilder()
+                .setColor("#000000")
+                .setTitle(`:${emoteName}:`)
+                .setThumbnail(fullUrl)
+                .setDescription("Emote successfully copied to server.");
+            
+            Logger.log(`Successfully uploaded emote :${emoteName}: (${emoteId}) to guild ${guild.name} (${guildId})`);
+            await args.interaction.editReply({ embeds: [ embed ] });
+        } catch (error: any) {
+            Logger.log(`Failed to upload emote :${emoteName}: to guild ${guild.name} (${guildId}) : ${error as string}`);
+            if (error instanceof DiscordAPIError) {
+                if (error.code === 30008) {
+                    await args.interaction.editReply({ content: "The server has reached it's emoji limit." });
+                } else if (error.code === 50035) {
+                    await args.interaction.editReply({ content: "The image is too large or has invalid dimensions." });
+                } else if (error.code === 50013) {
+                    await args.interaction.editReply({ content: "I lack the proper permission to add emotes." });
+                } else {
+                    await args.interaction.editReply({ content: "The upload failed due to an unexpected error." });
+                }
+            } else {
+                await args.interaction.editReply({ content: "There was an unexpected problem uploading the emote." });
+            }
         }
     }
 });
