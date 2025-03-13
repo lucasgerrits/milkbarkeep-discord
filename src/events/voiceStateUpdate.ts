41f5f398b71417a754042548484eaa9ef55e7dfa
lucasgerrits <lucasjgerrits@gmail.com>
2025-03-13 16:59:07 -0500

    Fix voiceStateUpdate disconnect notifs from mutes, streams, session changes

--- src/events/voiceStateUpdate.ts
+++ src/events/voiceStateUpdate.ts
@@ -11,20 +11,44 @@ export default new Event(
 
         const oldChannel: VoiceBasedChannel | null = oldState.channel;
         const newChannel: VoiceBasedChannel | null = newState.channel;
-        if (!oldChannel && !newChannel) return;
 
-        if (!oldChannel && newChannel) {
-            Logger.log(`${newChannel.guild.name} (${newChannel.guildId}): ${member.displayName} (${member.id}) joined voice channel ${newChannel.name} (${newChannel.id})`);
-            const embed: EmbedBuilder = new EmbedBuilder()
-                .setColor("Green")
-                .setAuthor({ name: `${member.displayName} joined voice`, iconURL: member.user.displayAvatarURL() });
-            await newChannel.send({ embeds: [ embed ] });
-        } else if (oldChannel) {
-            Logger.log(`${oldChannel.guild.name} (${oldChannel.guildId}): ${member.displayName} (${member.id}) left voice channel ${oldChannel.name} (${oldChannel.id})`);
-            const embed: EmbedBuilder = new EmbedBuilder()
-                .setColor("Red")
-                .setAuthor({ name: `${member.displayName} left voice`, iconURL: member.user.displayAvatarURL() });
-            await oldChannel.send({ embeds: [ embed ] });
+        const oldChannelId: string | null = oldState.channelId;
+        const newChannelId: string | null = newState.channelId;
+
+        const oldStreaming: boolean | null = oldState.streaming;
+        const newStreaming: boolean | null = newState.streaming;
+
+        const oldVideo: boolean | null = oldState.selfVideo;
+        const newVideo: boolean | null = newState.selfVideo;
+
+        const oldSessionId = oldState.sessionId;
+        const newSessionId = newState.sessionId;
+
+        // Joins and disconnects
+        if (oldChannelId !== newChannelId) {
+            // Tell new channel user joined
+            if (newChannel) {
+                Logger.log(`${newChannel?.guild.name}: ${member.displayName} joined voice channel ${newChannel?.name}`);
+                const embed: EmbedBuilder = new EmbedBuilder()
+                    .setColor("Green")
+                    .setAuthor({ name: `${member.displayName} joined voice`, iconURL: member.user.displayAvatarURL() });
+                await newChannel?.send({ embeds: [ embed ] });
+            }
+
+            // Tell old channel user switched to channel in same guild or disconnected
+            if ((oldChannel && newChannel) && (oldChannel.guildId === newChannel.guildId)) {
+                Logger.log(`${newChannel?.guild.name}: ${member.displayName} switched from ${oldChannel?.name} to ${newChannel?.name}`);
+                const embed: EmbedBuilder = new EmbedBuilder()
+                    .setColor("Orange")
+                    .setAuthor({ name: `${member.displayName} switched to 🔊${newChannel?.name}`, iconURL: member.user.displayAvatarURL() });
+                await oldChannel?.send({ embeds: [ embed ] });
+            } else if (oldChannel) {
+                Logger.log(`${oldChannel?.guild.name}: ${member.displayName} left voice channel ${oldChannel?.name}`);
+                const embed: EmbedBuilder = new EmbedBuilder()
+                    .setColor("Red")
+                    .setAuthor({ name: `${member.displayName} left voice`, iconURL: member.user.displayAvatarURL() });
+                await oldChannel?.send({ embeds: [ embed ] });
+            }
         }
     }
 );
