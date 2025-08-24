7992a375ece2008bbde94819e33c959cfbe68de7
lucasgerrits <lucasjgerrits@gmail.com>
2025-08-24 11:26:02 -0500

    Remove commented Scheduler code

--- src/core/Scheduler.ts
+++ src/core/Scheduler.ts
@@ -14,32 +14,11 @@ export class Scheduler {
     // https://en.wikipedia.org/wiki/Cron
     // "cron: minute hour dayofmonth month dayofweek"
     public async initialize(): Promise<void> {
-        /*
-        later.date.localTime();
-
-        const midnightSchedule = later.parse.cron("0 0 * * *");
-        const midnightInterval = later.setInterval(() => { this.midnightChecks() }, midnightSchedule);
-
-        const bskyFeedMinutes: number = 10;
-        const bskyFeedSchedule = later.parse.cron(`*\/${bskyFeedMinutes} * * * *`);
-        const bskyFeedInterval = later.setInterval(() => { this.clientRef.bsky.updateAllFeeds(bskyFeedMinutes); }, bskyFeedSchedule);
-
-        const raFeedMinutes: number = 10;
-        const raFeedSchedule = later.parse.cron(`*\/${raFeedMinutes} * * * *`);
-        const raFeedInterval = later.setInterval(() => { this.clientRef.ra.updateAllFeeds(raFeedMinutes); }, raFeedSchedule);
-
-        const raWeeklySchedule = later.parse.cron("58 17 * * 0");
-        const raWeeklyInteral = later.setInterval(() => { this.clientRef.ra.weeklyReport(); }, raWeeklySchedule);
-
-        const appletonCamSchedule = later.parse.cron("0 * * * *");
-        const appletonCamInterval = later.setInterval(() => { AppletonCam.sendToAll(this.clientRef); }, appletonCamSchedule);
-        */
-
         cron.schedule("0 0 * * *", () => { this.midnightChecks(); });
         cron.schedule("*/10 * * * *", () => { this.clientRef.bsky.updateAllFeeds(10); });
         cron.schedule("*/10 * * * *", () => { this.clientRef.ra.updateAllFeeds(10); });
         cron.schedule("58 17 * * 0", () => { this.clientRef.ra.weeklyReport(); });
-        cron.schedule("0 * * * *", () => { AppletonCam.sendToAll(this.clientRef); });
+        //cron.schedule("0 * * * *", () => { AppletonCam.sendToAll(this.clientRef); });
     }
 
     private midnightChecks(): void {
