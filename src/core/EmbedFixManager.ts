b6ce0d7f8ff1351711d47a1bc53218760f73262d
lucasgerrits <lucasjgerrits@gmail.com>
2025-10-02 19:28:57 -0500

    Update Instagram embed fix URL

--- src/core/EmbedFixManager.ts
+++ src/core/EmbedFixManager.ts
@@ -5,7 +5,7 @@ import type { EmbedFixUrls } from "../types/FeatureTypes";
 
 export class EmbedFixManager {
     private static readonly domainMap: Map<string, string> = new Map<string, string>([
-        ["instagram.com", "instagramez.com"],
+        ["instagram.com", "vxinstagram.com"],
         ["tiktok.com", "vxtiktok.com"],
         ["twitter.com", "fixupx.com"],
         ["x.com", "fixupx.com"]
